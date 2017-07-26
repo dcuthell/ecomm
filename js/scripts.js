16a30e9e947beca1973e0cd7c6321db7b151e676
dcuthell <dcc1447@gmail.com>
2017-07-25 21:46:16 -0700

    added all sorts of sweet backend functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
 //Business Logic
+
+//GLOBAL VARIABLE FOR TESTING
+mySite = new SiteManager();
+
+//CONSTRUCTORS
 function SiteManager(){
-  this.buyer = [];
+  this.buyers = [];
   this.goods = [];
+  this.accounts = [];
+  this.active;
+}
+
+function Buyer(name, id){
+  this.buyerName = name;
+  this.cart = [];
+  this.buyerID = id;
 }
 
 function Good(name, desc, quantity, price, imglink, id){
@@ -13,18 +26,115 @@ function Good(name, desc, quantity, price, imglink, id){
   this.goodID = id;
 }
 
-function testPassword(first, second) {
-  if (first===second) {
-    return true;
-  } else {
-    return false;
-  }
-}
-function NewAccount(first, last, userName, password) {
+function Account(first, last, userName, password, id) {
   this.first = first;
   this.last = last;
   this.userName = userName;
   this.password = password;
+  this.accountID = id;
+}
+
+//FUNCTION PROTOTYPES
+
+//addGood will create a new good and give it an ID before adding it to the array
+SiteManager.prototype.addGood = function(name, desc, quantity, price, imglink) {
+  var index = this.goods.length;
+  var newGood = new Good(name, desc, quantity, price, imglink, index);
+  this.goods.push(newGood);
+}
+
+//addBuyer will create a new buyer and give it an ID before adding it to the array
+SiteManager.prototype.addBuyer = function(name) {
+  var index = this.buyers.length;
+  var newBuyer = new Buyer(name, index);
+  this.buyers.push(newBuyer);
+}
+
+//addAccount will create a new account and give it an ID before adding it to the array
+//it was also control for casing of username and password
+SiteManager.prototype.addAccount = function(first, last, userName, password) {
+  var index = this.accounts.length;
+  userName = userName.toLowerCase();
+  password = password.toLowerCase();
+  var newAccount = new Account(first, last, userName, password, index);
+  this.accounts.push(newAccount);
+}
+
+SiteManager.prototype.goodToCart = function(buyerID, goodID, amount) {
+  if(this.goods[goodID].decreaseAmount(amount) === 0){
+    alert("Not enough inventory");
+    return 0;
+  }
+  this.buyers[buyerID].addToCart(this.goods[goodID], amount);
+}
+
+SiteManager.prototype.cartToGood = function(buyerID, goodID, amount) {
+  var oldGoodID = this.buyers[buyerID].cart[goodID].oldID;
+  if(this.buyers[buyerID].removeFromCart(goodID, amount) === 0){
+    alert("Not enough in cart");
+    return 0;
+  }
+  alert(oldGoodID);
+  this.goods[oldGoodID].increaseAmount(amount);
+}
+
+//addToCart will take a good, and amount, and create a new good item in the cart with the amount as the quantity. It will also assign a new ID for use in the cart. This allows us to "separate" a good into two different amounts.
+//Also will remove amount from good's inventory
+Buyer.prototype.addToCart = function(inputGood, amount){
+  var index = this.cart.length;
+  var newGood = new Good(inputGood.goodName, inputGood.goodDesc, amount, inputGood.price, inputGood.imgLink, index);
+  newGood.oldID = inputGood.goodID;
+  this.cart.push(newGood);
+}
+
+//removeFromCart will take a good index and an amount, and remove that many of that good from the cart. If successful, it will return that amount
+//If the amount is greater than the quantity, it will return 0. If the good's quantity goes to 0, it will remove that good from the cart
+Buyer.prototype.removeFromCart = function(goodIndex, amount){
+  if(this.cart[goodIndex].quantity < amount){
+    return 0;
+  }
+  if(this.cart[goodIndex].quantity === amount){
+    this.cart.splice(goodIndex, 1);
+    return amount;
+  }
+  this.cart[goodIndex].decreaseAmount(amount);
+  return amount;
+}
+
+//totalCart will add up all the prices of the goods in the cart and return to total
+Buyer.prototype.totalCart = function(){
+  var total = 0;
+  for(var i=0; i < this.cart.length; i++){
+    total += (this.cart[i].price * this.cart[i].quantity);
+  }
+  return total;
+}
+//decreaseAmount will take an amount input and subract that amount from a good's quantity
+//It will return that same amount, unless the amount is greater than the quantity, it which case it will return 0
+Good.prototype.decreaseAmount = function(amount){
+  var newAmount = this.quantity - amount;
+  if(newAmount < 0){
+    return 0;
+  }
+  this.quantity -= amount;
+  return amount;
+}
+
+//increaseAmount will take an amount input and add that amount to a good's quantity
+Good.prototype.increaseAmount = function(amount){
+  this.quantity += amount;
+  return 1;
+}
+
+
+//POPULATE FUNCTIONS
+//Enters in dummy data for testing purposes
+
+function populateBuyers(sitemanager){
+  var name = ["Jimmy the Nose", "Buddy Small Shoes", "Frankie Squats", "Donny Diamonds", "Fast Freddy"];
+  for (var i=0; i < name.length; i++){
+    sitemanager.addBuyer(name[i]);
+  }
 }
 
 function populateGoods(sitemanager){
@@ -39,23 +149,19 @@ function populateGoods(sitemanager){
   }
 }
 
-SiteManager.prototype.addGood = function(name, desc, quantity, price, imglink) {
-  var index = this.goods.length;
-  var newGood = new Good(name, desc, quantity, price, imglink, index);
-  this.goods.push(newGood);
-}
 
-Good.prototype.decreaseAmount = function(amount){
-  var newAmount = this.quantity - amount;
-  if(newAmount < 0){
-    alert("Not enough inventory");
-    return 0;
+
+function testPassword(first, second) {
+  if (first===second) {
+    return true;
+  } else {
+    return false;
   }
-  this.quantity -= amount;
-  return amount;
 }
 
-NewAccount.prototype.fullName = function() {
+
+
+Account.prototype.fullName = function() {
   // console.log('yo');
   return this.first + ' ' + this.last;
 }
@@ -118,7 +224,7 @@ $(document).ready(function() {
     var verified = testPassword(pswd, pswdConfirm);
     //If verified, create new Object
     if (verified) {
-      var accountHolder = new NewAccount(first, second, newUserName, pswd)
+      var accountHolder = new Account(first, second, newUserName, pswd)
       accountBank.push(accountHolder);
     } else {
       alert("Please enter a valid password");
